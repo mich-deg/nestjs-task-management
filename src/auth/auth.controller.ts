dc68e95766deba4480a6f82921d6ef946e091ad0
Mike <mike@tech.et>
2022-07-04 22:29:24 +0300

    jwt-authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,9 @@ export class AuthController {
   }
 
   @Post('/signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     // console.log(authCredentialsDto);
     return this.authService.signIn(authCredentialsDto);
   }
