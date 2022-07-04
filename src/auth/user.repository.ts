a95e2dfab32d45ed5b2041da3f36e430ea0b59cf
Mike <mike@tech.et>
2022-07-04 20:46:09 +0300

    hashing password and using salts

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,14 +6,22 @@ import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
+import * as bcrypt from 'bcrypt';
+
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async singUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    // const salt = await bcrypt.genSalt();
+    // console.log(salt);
+
     const user = new User();
     user.username = username;
-    user.password = password;
+    // user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+    // console.log(user.password);
 
     try {
       await user.save();
@@ -24,5 +32,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
